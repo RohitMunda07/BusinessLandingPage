7919288017382bb1aa13f6f566abc76e540e320e
RohitMunda07 <rohitkumar14818@gmail.com>
2025-06-02 14:22:15 +0530

    first commit

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector("#menu");
+const navBar = document.querySelector("#navBar");
+
+menu.addEventListener("click",()=>{
+    navBar.classList.toggle("hidden")
+})
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.htm*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
